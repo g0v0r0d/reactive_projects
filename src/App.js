80474a53b1403267c3376123a868a4b47e017ad0
qeeera <www.savitchgarik@yandex.ru>
2024-02-24 23:21:33 +0300

    quiz done

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function Result({correct}) {
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
       <a href='/'>
 
-      <button>Попробовать снова</button>2
+      <button>Попробовать снова</button>
       </a>
     </div>
   );
